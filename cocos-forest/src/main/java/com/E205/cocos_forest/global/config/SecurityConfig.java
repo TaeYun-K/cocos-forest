bae1740b66dbcb7cdf35b8b305aeb8164fcadb2e
minsoo <happyms817@gmail.com>
2025-09-13 07:10:28 +0900

    [INFRA] fix : index.html 확인

---
+++ cocos-forest/src/main/java/com/E205/cocos_forest/global/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.E205.cocos_forest.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // H2 콘솔 등 사용 시
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // ✅ 전체 허용
+            );
+        return http.build();
+    }
+}
