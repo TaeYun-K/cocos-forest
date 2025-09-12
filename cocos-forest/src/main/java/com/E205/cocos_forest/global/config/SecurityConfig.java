6aed9552050dfc2f8558a130ae006d7b53ca2fd1
minsoo <happyms817@gmail.com>
2025-09-13 08:27:04 +0900

    [INFRA] fix : .anyRequest().authenticated()

--- cocos-forest/src/main/java/com/E205/cocos_forest/global/config/SecurityConfig.java
+++ cocos-forest/src/main/java/com/E205/cocos_forest/global/config/SecurityConfig.java
@@ -20,14 +20,6 @@ public class SecurityConfig {
                 .requestMatchers("/css/**", "/js/**", "/favicon.ico").permitAll()
                 // 그 외 모든 요청은 반드시 인증(로그인)을 거쳐야 함
                 .anyRequest().authenticated()
-            )
-
-            // [핵심 설정] 폼 기반 로그인을 사용하도록 설정합니다.
-            .formLogin(form -> form
-                // 우리가 사용할 로그인 페이지의 경로를 지정합니다.
-                .loginPage("/login")
-                // 로그인 성공 시 이동할 기본 경로를 지정합니다.
-                .defaultSuccessUrl("/", true)
             );
 
         return http.build();
