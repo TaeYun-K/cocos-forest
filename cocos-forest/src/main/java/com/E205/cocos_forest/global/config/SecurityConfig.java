9515a0c39e3ca1ffa2126ff7c758a7f38aecf1ab
minsoo <happyms817@gmail.com>
2025-09-13 10:50:25 +0900

    [INFRA] fix: .formLogin(withDefaults()) 추가

--- cocos-forest/src/main/java/com/E205/cocos_forest/global/config/SecurityConfig.java
+++ cocos-forest/src/main/java/com/E205/cocos_forest/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.E205.cocos_forest.global.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,8 +17,10 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // H2 콘솔 등 사용 시
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // ✅ 전체 허용
-            );
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults())
+            .logout(withDefaults());
         return http.build();
     }
 }
