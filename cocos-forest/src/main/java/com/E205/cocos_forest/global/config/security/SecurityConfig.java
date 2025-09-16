9c6ca5fd53a021b0c99734701d48242a60508b25
DESKTOP-DLOPF19\SSAFY <happyms817@gmail.com>
2025-09-16 16:53:17 +0900

    [BE] feat: login 구현 - access, refresh token

--- cocos-forest/src/main/java/com/E205/cocos_forest/global/config/security/SecurityConfig.java
+++ cocos-forest/src/main/java/com/E205/cocos_forest/global/config/security/SecurityConfig.java
@@ -1,21 +1,25 @@
-package com.E205.cocos_forest.global.config;
+package com.E205.cocos_forest.global.config.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.E205.cocos_forest.global.jwt.JwtAuthenticationFilter;
+import com.E205.cocos_forest.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
     /**
      * passwordEncoder(): BCryptPasswordEncoder를 Spring 컨테이너에 Bean으로 등록합니다.
      * 이렇게 등록해두면 다른 곳에서 @Autowired나 생성자 주입으로 가져다 쓸 수 있습니다.
@@ -30,11 +34,11 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (API 서버에서는 보통 비활성화)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안함 (JWT 기반 인증)
-            .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+
             .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/**").permitAll()
             )
-            .csrf((csrf) -> csrf.disable());
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
