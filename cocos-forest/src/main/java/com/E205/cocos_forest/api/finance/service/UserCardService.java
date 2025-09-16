9b0336802cef6b4c243beff604f3821384eb658b
DESKTOP-DLOPF19\SSAFY <kty8600@naver.com>
2025-09-16 14:50:25 +0900

    [BE] refactor : finance 폴더 정리 계좌, 은행, 카드 기능별로 폴더 세분화

--- cocos-forest/src/main/java/com/E205/cocos_forest/api/finance/service/UserCardService.java
+++
@@ -1,9 +0,0 @@
-package com.E205.cocos_forest.api.finance.service;
-
-import com.E205.cocos_forest.api.finance.dto.in.CardLinkCreateIn;
-import com.E205.cocos_forest.api.finance.dto.out.CardLinkOut;
-
-public interface UserCardService {
-    CardLinkOut linkCard(CardLinkCreateIn in);
-}
-
