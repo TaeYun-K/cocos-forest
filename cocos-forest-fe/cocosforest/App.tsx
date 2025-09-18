d80cb5792d5708f37fa219c2f72cd804ed8912d4
권인 <in24041210@gmail.com>
2025-09-18 15:10:03 +0900

    [FE] fix: 코드 중복 오류 수정

--- cocos-forest-fe/cocosforest/App.tsx
+++ cocos-forest-fe/cocosforest/App.tsx
@@ -1,15 +1,11 @@
 // App.tsx
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RootNavigator } from "./src/navigation/RootNavigator";
 
 const queryClient = new QueryClient();
-import { useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import { RootNavigator } from "./src/navigation/RootNavigator";
 
 export default function App() {
   useEffect(() => {
